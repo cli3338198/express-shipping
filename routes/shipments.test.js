c3ab389fd3031226f970476a6df1fc1e089c76c4
Christopher Lim <cli3338198@gmail.com>
2022-11-08 17:33:45 -0800

    finished test

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,14 @@
 "use strict";
+const shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
 
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(1001);
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,30 +16,25 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 1001 });
   });
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
   });
 
   test("throws error if invalid id & missing name", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          "productId": 100,
-          "addr": "SOME ADDR",
-          "zip": "SOME ZIP"
-        });
+    const resp = await request(app).post("/shipments").send({
+      productId: 100,
+      addr: "SOME ADDR",
+      zip: "SOME ZIP",
+    });
 
     expect(resp.statusCode).toEqual(400);
     expect(resp.body.error.message).toEqual([
       "instance.productId must be greater than or equal to 1000",
-			"instance requires property \"name\""
-    ])
-  })
+      'instance requires property "name"',
+    ]);
+  });
 });
