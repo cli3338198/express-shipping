0cc9d383bae4549454381ccf5785342e888b0b17
Christopher Lim <cli3338198@gmail.com>
2022-11-08 16:18:43 -0800

    started mock test

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,56 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const { shipProduct } = require("./shipItApi");
 
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+const BASE_URL = "http://localhost:3001/ship";
+const SHIPIT_API_KEY = "SUPER-DUPER-SECRET";
 
 test("shipProduct", async function () {
-  const shipId = await shipProduct({
-    productId: 1000,
-    name: "Test Tester",
-    addr: "100 Test St",
-    zip: "12345-6789",
+  axiosMock.onPost(`${BASE_URL}`).reply(200, {
+    receipt: {
+      itemId: 1001,
+      name: "SOME THING",
+      addr: "SOME ADDR",
+      zip: "SOME ZIP",
+      shipId: 1,
+    },
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  const shipId = await shipProduct({
+    productId: 1001,
+    name: "SOME THING",
+    addr: "SOME ADDR",
+    zip: "SOME ZIP",
+  });
+
+  expect(shipId).toEqual(1);
 });
+
+test("shipProduct with bad data", async function () {
+  axiosMock.onPost(`${BASE_URL}`).reply(200, {
+    receipt: {
+      itemId: 1001,
+      name: "SOME THING",
+      addr: "SOME ADDR",
+      zip: "SOME ZIP",
+      shipId: 1,
+    },
+  });
+
+  const shipId = await shipProduct({
+    productId: 1001,
+    name: "SOME THING",
+    addr: "SOME ADDR",
+    zip: "SOME ZIP",
+  });
+
+  expect(shipId).toEqual(1);
+});
+// test("fact about 7", async function () {
+//   axiosMock.onGet(`${BASE_URL}/7`).reply(200, { fact: "7 is lucky" });
+//   const res = await getFact(7);
+//   expect(res).toEqual("7 is lucky");
+// });
--- shipit-server.js
+++ shipit-server.js
@@ -2,14 +2,12 @@
 
 // YOU SHOULD NOT BE READING THIS FILE. GO AWAY, CHEATER.
 
-
 /** Server for shipit. */
 
 const express = require("express");
 const { NotFoundError, BadRequestError } = require("./expressError");
 const cors = require("cors");
 require("colors");
-
 
 const app = new express();
 
@@ -23,7 +21,7 @@ app.post("/ship", function (req, res, next) {
   const { itemId, name, addr, zip, key } = req.body;
   if (key !== "SUPER-DUPER-SECRET") {
     console.error("API user didn't send valid key!".red);
-    return res.json({"error": "missing key"});
+    return res.json({ error: "missing key" });
   }
   console.log("got: ".yellow, req.body);
   const shipId = Math.floor(Math.random() * 10000) + 1000;
@@ -33,7 +31,6 @@ app.post("/ship", function (req, res, next) {
 
   return res.json({ receipt });
 });
-
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
@@ -47,8 +44,6 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
-
 
 app.listen(3001, function () {
   console.log("ShipIt server started at http://localhost:3001".yellow);
